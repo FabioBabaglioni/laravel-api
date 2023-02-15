579d78fc43e9ba7c432850406c4e475282c3f7f7
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-02-15 15:31:41 +0100

    Creo tabelle db

--- database/migrations/2023_02_15_142400_create_movies_table.php
+++ database/migrations/2023_02_15_142400_create_movies_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 32);
+            $table->date('year');
+            $table->integer('cashOut');
+
             $table->timestamps();
         });
     }
