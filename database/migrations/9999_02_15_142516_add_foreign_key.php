cb33b0bb68442e492cab5f21cbfaff6f1ce80683
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-02-15 15:42:08 +0100

    Creo relazioni

--- database/migrations/9999_02_15_142516_add_foreign_key.php
+++ database/migrations/9999_02_15_142516_add_foreign_key.php
@@ -13,7 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        
+        // relazione una a molti 
+        Schema::table('movies', function (Blueprint $table) {
+            $table -> foreignId('genre_id')
+                   -> constrained();
+        });
+
+        // relazione molti a molti
+        Schema::table('movie_tag', function (Blueprint $table) {
+            $table -> foreignId('movie_id')
+                   -> constrained();
+
+            $table -> foreignId('tag_id')
+                   -> constrained();
+        });
     }
 
     /**
@@ -23,6 +36,17 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('movies', function (Blueprint $table) {
+            $table -> dropForeign(['genre_id']);
+            $table -> dropColumn('genre_id');
+        });
+
+        Schema::table('movie_tag', function (Blueprint $table) {
+            $table -> dropForeign(['movie_id']);
+            $table -> dropColumn('movie_id');
+
+            $table -> dropForeign(['tag_id']);
+            $table -> dropColumn('tag_id');
+        });
     }
 };
